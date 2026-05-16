5d216e2dddca4d85be9c3fa57db70702bffe16ca
Shiar owo <jhairtoc@gmail.com>
2026-05-16 17:14:51 -0500

    chore (CI/CD): add folder for GitHub actions files, one for each microservice - In auth-service: the main test was commented due to unnecesary file

--- auth-service/src/test/java/com/rapidocurier/authservice/AuthServiceApplicationTests.java
+++ auth-service/src/test/java/com/rapidocurier/authservice/AuthServiceApplicationTests.java
@@ -1,15 +1,15 @@
-package com.rapidocurier.authservice;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
-
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
-class AuthServiceApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.rapidocurier.authservice;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.context.annotation.Import;
+//
+//@Import(TestcontainersConfiguration.class)
+//@SpringBootTest
+//class AuthServiceApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
