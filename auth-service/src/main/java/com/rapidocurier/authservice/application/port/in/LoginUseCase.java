713a77beee33742c6312f3693ee202e1750eea41
Shiar owo <jhairtoc@gmail.com>
2026-05-16 16:36:42 -0500

    feat (auth-service/application): add Login and Register use cases

---
+++ auth-service/src/main/java/com/rapidocurier/authservice/application/port/in/LoginUseCase.java
@@ -0,0 +1,5 @@
+package com.rapidocurier.authservice.application.port.in;
+
+public interface LoginUseCase {
+    String login(String email, String password);
+}
---
+++ auth-service/src/main/java/com/rapidocurier/authservice/application/port/in/RegisterUseCase.java
@@ -0,0 +1,7 @@
+package com.rapidocurier.authservice.application.port.in;
+
+import com.rapidocurier.authservice.domain.model.Usuario;
+
+public interface RegisterUseCase {
+    Usuario registrar(String nombre, String email, String password);
+}
