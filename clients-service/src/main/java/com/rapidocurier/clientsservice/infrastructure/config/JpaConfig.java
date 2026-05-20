2287994c8ab27b3984c5aa9dd1afcc310e45caba
Shiar owo <jhairtoc@gmail.com>
2026-05-20 17:52:53 -0500

    refactor(clients-service/infrastructure): add extra configuration

--- clients-service/src/main/java/com/rapidocurier/clientsservice/infrastructure/config/JpaConfig.java
+++ clients-service/src/main/java/com/rapidocurier/clientsservice/infrastructure/config/JpaConfig.java
@@ -1,8 +1,6 @@
 package com.rapidocurier.clientsservice.infrastructure.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @Configuration
-@EnableJpaAuditing
 public class JpaConfig {}
