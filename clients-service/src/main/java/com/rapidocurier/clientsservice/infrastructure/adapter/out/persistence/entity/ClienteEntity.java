1761733b968ef547db14de2103725271926d04ed
Shiar owo <jhairtoc@gmail.com>
2026-05-20 16:37:17 -0500

    feat(clients-service/infrastructure): add Persistence Entity (ClienteEntity) and fields with Data Auditing (createdAt and updatedAt)

---
+++ clients-service/src/main/java/com/rapidocurier/clientsservice/infrastructure/adapter/out/persistence/entity/ClienteEntity.java
@@ -0,0 +1,54 @@
+package com.rapidocurier.clientsservice.infrastructure.adapter.out.persistence.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "clientes")
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClienteEntity {
+
+    @Id
+    private UUID id;
+
+    @Column(name = "dni", nullable = false, unique = true)
+    private String dni;
+
+    @Column(name = "nombre", nullable = false)
+    private String nombre;
+
+    @Column(name = "apellido_paterno", nullable = false)
+    private String apellidoPaterno;
+
+    @Column(name = "apellido_materno", nullable = false)
+    private String apellidoMaterno;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private OffsetDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private OffsetDateTime updatedAt;
+
+}
