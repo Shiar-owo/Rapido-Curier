8bb4dd61b92fdca921611d7596c9334d306e87da
Shiar owo <jhairtoc@gmail.com>
2026-05-20 17:06:57 -0500

    feat(clients-service/infrastructure): add JpaRepository Interface

---
+++ clients-service/src/main/java/com/rapidocurier/clientsservice/infrastructure/adapter/out/persistence/repository/ClienteJpaRepository.java
@@ -0,0 +1,20 @@
+package com.rapidocurier.clientsservice.infrastructure.adapter.out.persistence.repository;
+
+import com.rapidocurier.clientsservice.infrastructure.adapter.out.persistence.entity.ClienteEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface ClienteJpaRepository extends JpaRepository<ClienteEntity, UUID> {
+
+    Optional<ClienteEntity> findByDni(String dni);
+
+    Optional<ClienteEntity> findByEmail(String email);
+
+    boolean existsByDni(String dni);
+
+    boolean existsByEmail(String email);
+}
