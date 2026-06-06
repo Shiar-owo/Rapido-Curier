b821a59354f0414516af3c0848f1211617157ff6
Shiar owo <jhairtoc@gmail.com>
2026-06-06 12:27:23 -0500

    feat(paquetes-service/domain): add domain exceptions could occur in application layer

---
+++ paquetes-service/src/main/java/com/rapidocurier/paquetesservice/domain/exception/ConflictException.java
@@ -0,0 +1,5 @@
+package com.rapidocurier.paquetesservice.domain.exception;
+
+public class ConflictException extends RuntimeException {
+    public ConflictException(String message) { super(message); }
+}
